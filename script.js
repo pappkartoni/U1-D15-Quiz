5f0844b8cada086a302e7752d9cbd6e2e7389606
Florian Dreyer <flodreyer@web.de>
2022-12-09 14:40:57 +0100

    setup page with at least two quizzes (hardcoded)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const questions = [
+const sciencequiz = [
     {
       category: "Science: Computers",
       type: "multiple",
@@ -98,11 +98,85 @@ const questions = [
     },
 ];
 
-/* 
+const doomquiz = [
+    {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "medium",
+        question: "What is MF Doom's real name?",
+        correct_answer: "Daniel Dumile",
+        incorrect_answers: ["Dwayne Michael Carter Jr.", "Shawn Carter", "Otis Jackson Jr."],
+      },
+      {
+        category: "DOOM",
+        type: "boolean",
+        difficulty: "easy",
+        question: "MF Doom is still alive.",
+        correct_answer: "False",
+        incorrect_answers: ["True"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "medium",
+        question: "When did MF Doom release his first solo studio album 'Operation Doomsday'",
+        correct_answer: "1999",
+        incorrect_answers: ["1997", "1998", "2000"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "easy",
+        question: "With whom did MF Doom collaborate on the 2004 LP 'Madvillainy'?",
+        correct_answer: "Madlib",
+        incorrect_answers: ["Bishop Nehru", "Czarface", "Danny Brown"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "medium",
+        question: "Which of these is NOT a pseudonym of MF Doom?",
+        correct_answer: "Quasimoto",
+        incorrect_answers: ["Viktor Vaughn", "Zev Love X", "King Geedorah"],
+      },
+      {
+        category: "DOOM",
+        type: "boolean",
+        difficulty: "easy",
+        question: "Has MF Doom ever featured on a Gorillaz album?",
+        correct_answer: "True",
+        incorrect_answers: ["False"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "medium",
+        question: "What was the name of the group MF Doom formed with his brother in 1988",
+        correct_answer: "KMD",
+        incorrect_answers: ["RZA", "CREAM", "WTF"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "hard",
+        question: "What was the title of their only commercially released album?",
+        correct_answer: "Mr. Hood",
+        incorrect_answers: ["Young, Broke & Infamous", "Reasonabable Doubt", "Playtime is over"],
+      },
+      {
+        category: "DOOM",
+        type: "multiple",
+        difficulty: "hard",
+        question: "MF Doom's signature metal mask is based on a prop of which movie?",
+        correct_answer: "Gladiator",
+        incorrect_answers: ["300", "Troy", "Spartacus"],
+      },
+];/* 
 
  ----- GLOBAL VARIABLES ----- 
 
  */
+let questions;
 let questionNumber = 0;
 let points = 0;
 let container = document.getElementById("container");
@@ -115,7 +189,8 @@ let container = document.getElementById("container");
 function restart() {
     questionNumber = 0;
     points = 0;
-    renderQuestion();
+    document.getElementById("buttonbar").innerHTML = "";
+    renderSetup();
 }
 
 function nextQuestion() { 
@@ -138,6 +213,7 @@ function nextQuestion() {
 function renderQuestion() {
     container.innerHTML = "";
     let question =  questions[questionNumber];
+    console.log(question);
     let qn = document.createElement("div");
     qn.classList.add("question");
     
@@ -182,6 +258,8 @@ function renderResult() {
     evaluation.innerHTML = `<p>You did great! You scored ${points} out of a total of ${questions.length} points!</p>`
     res.appendChild(evaluation);
     
+    let bbar = document.getElementById("buttonbar"); //TODO: Find better solution
+    bbar.removeChild(bbar.lastChild);
     container.appendChild(res);
 }
 
@@ -251,11 +329,57 @@ function shuffleArray(arr) { // i reused this from yesterday
     return arr;
 }
 
+
+function newQuiz() {
+    let quizname = document.querySelector("select").value;
+    switch(quizname) {
+        case "science":
+            questions = sciencequiz;
+            break;
+        case "doom":
+            questions = doomquiz;
+            break;
+        default: // Idk, this won't really happen here but wanted to have something at least
+            questions = sciencequiz;
+    }
+    renderButtons(); // This is a problem if you somehow manage to make an invalid choice
+    renderQuestion();
+}
+
+function renderButtons() {
+    let buttonbar = document.getElementById("buttonbar");
+    buttonbar.innerHTML = `<input type="button" value="Restart!" onclick="restart()" />
+                           <input type="button" value="Next Question!" onclick="nextQuestion()" />`;
+}
+
+function renderSetup() {
+    container.innerHTML = "";
+    let bbar = document.getElementById("buttonbar");
+    bbar.innerHTML = "";
+    let stp = document.createElement("div");
+    stp.classList.add("setup");
+    
+    let h2 = document.createElement("h2");
+    h2.innerText = "What do you want play?";
+    stp.appendChild(h2);
+    let hr = document.createElement("hr");
+    stp.appendChild(hr);
+
+    let select = document.createElement("div");
+    select.innerHTML = `<label for="quizzes">Select a quiz:</label>
+                        <select id="quizzes" name="quizzes">
+                        <option value="science">Science Quiz</option>
+                        <option value="doom">MF DOOM</option>
+                        </select>
+                        <input type="button" value="Let's go!" onclick="newQuiz()">`;
+    stp.appendChild(select);
+    container.appendChild(stp);
+}
 /* 
 
  ----- ON LOAD ----- 
  
  */
 window.onload = function () {
-    renderQuestion(); //TODO let player have choice of quiz, i.e. implement renderSetup() somehow
+    renderSetup(); //TODO let player have choice of quiz, i.e. implement renderSetup() somehow
 };
