be6b79553e8efafe5e5f0782eba9f602f222543e
Florian Dreyer <flodreyer@web.de>
2022-12-09 13:15:04 +0100

    code cleanup

--- script.js
+++ script.js
@@ -98,102 +98,20 @@ const questions = [
     },
 ];
 
+/* 
+
+ ----- GLOBAL VARIABLES ----- 
+
+ */
 let questionNumber = 0;
 let points = 0;
 let container = document.getElementById("container");
 
-
-function renderQuestion() {
-    container.innerHTML = "";
-    let question =  questions[questionNumber];
-    let qn = document.createElement("div");
-    qn.classList.add("question");
-    
-    let h2 = document.createElement("h2");
-    h2.innerText = question.question;
-    qn.appendChild(h2);
-    let category = document.createElement("span");
-    category.innerText = "Category: " + question.category;
-    qn.appendChild(category);
-    let difficulty = document.createElement("span");
-    difficulty.innerText = "Difficulty: " + question.difficulty;
-    qn.appendChild(difficulty);
-
-    // Do different things depending on question type
-    if (question.type === "multiple") {
-        let answers = shuffleArray([question.correct_answer].concat(question.incorrect_answers)); // TODO: find better way to do this
-        renderMultipleQuestion(qn, answers);
-    } else if (question.type === "boolean") {
-        renderBoolQuestion(qn);
-    }
-
-    container.appendChild(qn);
-    let radios = document.querySelectorAll("input[type='radio'");
-    for (r of radios) {
-        r.addEventListener("change", checkAnswer)
-    }
-}
-
-function renderMultipleQuestion(qn, answers) {
-    for (let i = 0; i < answers.length; i++) {
-        let radio = generateRadioButton(answers[i], i);
-        qn.appendChild(radio);
-    }
-}
-
-function renderBoolQuestion(qn) {
-    qn.appendChild(generateRadioButton("True", 0));
-    qn.appendChild(generateRadioButton("False", 1));
-}
-
-function generateRadioButton(text, num) {
-    let radio = document.createElement("div");
-    radio.innerHTML = `<input type="radio" id="${num}" value="${text}" name="q${questionNumber}">
-                     <label for="${num}">${text}</label>`;
-    return radio;
-}
 /* 
-function checkListener(event) {
-    if (event.target.value === questions[questionNumber].correct_answer) {
-        event.target.parentElement.classList.add("correct");
-        points++;
-    } else {
-        event.target.parentElement.classList.add("wrong");
-    }
-}
+
+ ----- BUTTON FUNCTIONALITY ----- 
+
  */
-function checkAnswer(event) {
-    let radios = document.querySelectorAll("input[type='radio']:not(:checked)");
-    let choice = event.target
-    let answer = choice.value;
-    let corrAnswer = questions[questionNumber].correct_answer;
-    event.target.disabled = true;
-    if (answer === corrAnswer) {
-        choice.parentElement.classList.add("correct");
-        points++;
-    } else {
-        choice.parentElement.classList.add("wrong");
-    }
-
-    for (let r of radios) { // TODO rethink this whole thing
-        r.disabled = true;
-        if (r.value === corrAnswer) {
-            r.parentElement.classList.add("hint");
-        }
-    }
-}
-
-function shuffleArray(arr) { // i reused this from yesterday
-    for (let i=0;i<arr.length-1; i++) {
-        let j = Math.floor(Math.random() * (arr.length - i)) + i;
-        let swp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = swp;
-    }
-
-    return arr;
-}
-
 function restart() {
     questionNumber = 0;
     points = 0;
@@ -212,37 +130,134 @@ function nextQuestion() {
     }
 }
 
+/* 
+
+ ----- RENDER FUNCTIONS ----- 
+ 
+ */
+function renderQuestion() {
+    container.innerHTML = "";
+    let question =  questions[questionNumber];
+    let qn = document.createElement("div");
+    qn.classList.add("question");
+    
+    let h2 = document.createElement("h2");
+    h2.innerText = question.question;
+    qn.appendChild(h2);
+    let category = document.createElement("span");
+    category.innerText = "Category: " + question.category;
+    qn.appendChild(category);
+    let difficulty = document.createElement("span");
+    difficulty.innerText = "Difficulty: " + question.difficulty;
+    qn.appendChild(difficulty);
+    
+    // Do different things depending on question type
+    if (question.type === "multiple") {
+        let answers = shuffleArray([question.correct_answer].concat(question.incorrect_answers)); // TODO: find better way to do this
+        renderMultipleQuestion(qn, answers);
+    } else if (question.type === "boolean") {
+        renderBoolQuestion(qn);
+    }
+    
+    container.appendChild(qn);
+    let radios = document.querySelectorAll("input[type='radio'");
+    for (r of radios) {
+        r.addEventListener("change", checkAnswer)
+    }
+}
+
 function renderResult() {
     container.innerHTML = "";
     let res = document.createElement("div");
     res.classList.add("question");
-
+    
     let h2 = document.createElement("h2");
     h2.innerText = "Thank you for playing!";
     res.appendChild(h2);
-
+    
     let evaluation = document.createElement("div")
     evaluation.innerHTML = `<p>You did great! You scored ${points} out of a total of ${questions.length} points!</p>`
     res.appendChild(evaluation);
-
+    
     container.appendChild(res);
+}
+
+/* 
+
+ ----- QUESTION RENDERING SUBROUTINES ----- 
+ 
+ */
+function renderMultipleQuestion(qn, answers) {
+    for (let i = 0; i < answers.length; i++) {
+        let radio = generateRadioButton(answers[i], i);
+        qn.appendChild(radio);
+    }
+}
+
+function renderBoolQuestion(qn) {
+    qn.appendChild(generateRadioButton("True", 0));
+    qn.appendChild(generateRadioButton("False", 1));
+}
+
+function generateRadioButton(text, num) {
+    let radio = document.createElement("div");
+    radio.innerHTML = `<input type="radio" id="${num}" value="${text}" name="q${questionNumber}">
+                     <label for="${num}">${text}</label>`;
+    return radio;
+}
+
+/* 
+
+ ----- CHECKER FUNCTION ----- 
+ 
+ */
+function checkAnswer(event) { // TODO rethink this whole thing
+    let radios = document.querySelectorAll("input[type='radio']:not(:checked)");
+    let choice = event.target
+    let answer = choice.value;
+    let corrAnswer = questions[questionNumber].correct_answer;
+    event.target.disabled = true;
+    if (answer === corrAnswer) {
+        choice.parentElement.classList.add("correct");
+        points++;
+    } else {
+        choice.parentElement.classList.add("wrong");
+    }
+
+    for (let r of radios) {
+        r.disabled = true;
+        if (r.value === corrAnswer) {
+            r.parentElement.classList.add("hint");
+        }
+    }
+}
+/* 
+function checkListener(event) {
+    if (event.target.value === questions[questionNumber].correct_answer) {
+        event.target.parentElement.classList.add("correct");
+        points++;
+    } else {
+        event.target.parentElement.classList.add("wrong");
+    }
+}
+ */
+
+/* 
+
+ ----- HELPER ----- 
+ 
+ */
+function shuffleArray(arr) { // i reused this from yesterday
+    for (let i=0;i<arr.length-1; i++) {
+        let j = Math.floor(Math.random() * (arr.length - i)) + i;
+        let swp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = swp;
+    }
+
+    return arr;
 }
 
 window.onload = function () {
     renderQuestion();
-    // HINTS
-    // IF YOU ARE DISPLAYING ALL THE QUESTIONS AT ONCE:
-    // For each question, create a container for wrapping it; then create a radio button
-    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-    // with, as options, both the correct answer and the incorrect ones
-    // (you'll probably need to google how to get the value from a radio button in JS to evaluate the final score)
-    //
-    // IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
-    // Display the first question with the text and the radio buttons
-    // when the user selects an answer, pick the next question from the array and replace the old one with it
-    // saving the user's choice in a variable
-  };
-
-  // How to calculate the result? You can do it in 2 ways:
-  // If you are presenting all the questions together, just take all the radio buttons and check if the selected answer === correct_answer
-  // If you are presenting one question at a time, just add one point or not to the user score if the selected answer === correct_answer
+};
